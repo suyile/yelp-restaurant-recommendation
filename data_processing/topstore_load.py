0359757f9c6edbe2074cf0707de33b692d6ababb
suyile <suyile@gmail.com>
2021-05-25 22:47:50 -0400

    processing file

--- data_processing/topstore_load.py
+++ data_processing/topstore_load.py
@@ -1,30 +1,25 @@
 from pyspark import SparkConf, SparkContext
 import sys
 from pyspark.sql.types import StructType, StructField, StringType
-from pyspark.sql import SQLContext,DataFrame,Row
+from pyspark.sql import SQLContext,DataFrame, Row
 
+"""
+Another way to set up jar for spark conf
+Please check spark official dataframe: https://spark.apache.org/docs/latest/sql-data-sources-jdbc.html
+conf.set("spark.executor.extraClassPath", "./jar/sqlite-jdbc-3.8.6.jar")
+conf.set("spark.driver.extraClassPath", "./jar/sqlite-jdbc-3.8.6.jar")
+conf.set("spark.jars", "./jar/sqlite-jdbc-3.8.6.jar")
+"""
 
-
-#.set("spark.executor.extraClassPath","/Users/suyile/Documents/yelp/ETL/sqlite-jdbc-3.8.6.jar")\
-#.set("spark.driver.extraClassPath","/Users/suyile/Documents/yelp/ETL/sqlite-jdbc-3.8.6.jar")\
-#.set("spark.jars","/Users/suyile/Documents/yelp/ETL/sqlite-jdbc-3.8.6.jar")
-
-#${SPARK_HOME}/bin/spark-submit --master=local --driver-class-path ./sqlite-jdbc-3.8.6.jar --jars ./sqlite-jdbc-3.8.5.jar topstore_load.py
-
-
-
+# Define the Schema of Table
 storeSchema = StructType([
     StructField('city', StringType(), False),
-    StructField('season',StringType(),False),
-    StructField('store_id',StringType(),False),
-    StructField('store_name',StringType(),False),
+    StructField('season', StringType(), False),
+    StructField('store_id', StringType(), False),
+    StructField('store_name', StringType(), False),
 ])
 
-conf = SparkConf().setAppName('Euler')
-sc = SparkContext(conf=conf)
-sqlContext = SQLContext(sc)
-input=sys.argv[1]
-#output=sys.argv[2]
+# store = Row('city','season','store_id','store_name')
 
 def state_to_city(state):
     return {
@@ -40,31 +35,47 @@ def state_to_city(state):
          'WI':'Madison',
     }[state]
 
-store=Row('city','season','store_id','store_name')
 
-def line_to_object(state,season,stores):
-    store_list=stores.strip().split('|')
-    res=[]
+
+def line_to_object(state, season, stores):
+    store_list = stores.strip().split('|')
+    res = []
     city = state_to_city(state)
     for i in xrange(0, len(store_list)):
-        store=store_list[i]
+        store = store_list[i]
         s = store.strip().split(',')
-        store_id=s[0]
-        store_name=s[1].encode('ascii', 'ignore')
-        store=(city,season,store_id,store_name)
+        store_id = s[0]
+        store_name = s[1].encode('ascii', 'ignore')
+        store = (city, season, store_id, store_name)
         res.append(store)
 
     return res
 
-res=sc.textFile(input).map(lambda line : line.strip().split('::')).\
-    map(lambda line : (line[0],line[1],line[2].encode('ascii', 'ignore')))\
-    .flatMap(lambda (state, season, stores):line_to_object(state, season, stores))
+def main():
+    ## Taking the input txt file for processing
+    input = sys.argv[1]
 
-url="jdbc:sqlite:/Users/suyile/Documents/yelp/web/db.sqlite3 "
-table="recommend_stores"
+    conf = SparkConf().setAppName('data_processing')
+    sc = SparkContext(conf=conf)
+    sqlContext = SQLContext(sc)
 
-df = sqlContext.createDataFrame(res, storeSchema)
-df.write.jdbc(url, table, 'overwrite')
-df.show()
+    res = sc.textFile(input) \
+        .map(lambda line: line.strip().split('::')) \
+        .map(lambda line: (line[0], line[1], line[2].encode('ascii', 'ignore'))) \
+        .flatMap(lambda (state, season, stores): line_to_object(state, season, stores))
 
-df.saveAsTextFile(output)
+    df = sqlContext.createDataFrame(res, storeSchema)
+
+    # Connect to database and write the result to sqlite db
+    url = "jdbc:sqlite:/Users/suyile/Documents/yelp/web/db.sqlite3 "
+    table = "recommend_stores"
+    df.write.jdbc(url, table, 'overwrite')
+
+    # Save sample output to txt file
+    # output=sys.argv[2]
+    # df.saveAsTextFile(output)
+
+
+
+if __name__=="__main__":
+    main()
